7db6e491d3d891886c0c7b2c7f9c925d8f6df79e
juvenalelias <juvenalelias@gmail.com>
2020-11-05 15:09:27 -0500

    me ha mamado gallo el modelo one to many

--- graphql/resolvers/applications.js
+++ graphql/resolvers/applications.js
@@ -1,6 +1,7 @@
 const db = require("../../models");
 const helpers = require("../../helpers");
 const Application = db.application;
+const Campos = db.campo;
 
 module.exports = {
     applications: async (args) => {
@@ -16,5 +17,23 @@ module.exports = {
         } catch (error) {
             throw error;
         }
-    }
+    },
+
+    saveAppFields: async (args) => {
+        try {
+            const { application, campos } = args.application
+            let newFields = campos.map(field => {
+                return {
+                    ...field,
+                    applicationId: application.id,
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString()
+                }
+            });
+            return campos = await Campos.insertMany(newFields);
+//            return { ...newObj._doc, _id: newObj.id }
+        } catch (error) {
+            throw error;
+        }
+    },
 };
--- models/index.js
+++ models/index.js
@@ -21,4 +21,5 @@ db.etiquetas = require("../models/etiquetas")(sequelize, Sequelize);
 db.tipocampos = require("../models/tipocampos")(sequelize, Sequelize);
 db.mascara     = require("../models/mascara")(sequelize, Sequelize);
 db.application = require("../models/applications")(sequelize, Sequelize);
+db.campo = require("../models/campos")(sequelize, Sequelize);
 module.exports = db;
