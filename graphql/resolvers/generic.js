e1284ad7f792a76caa0cfd183bd05597bfe152a5
andresverjan <andresverjan@hotmail.com>
2020-11-07 15:52:04 -0500

    Creating GENERICSAVE

--- graphql/resolvers/generic.js
+++ graphql/resolvers/generic.js
@@ -25,5 +25,38 @@ module.exports = {
             throw error;
         }
     },
+    genericSave: async (args) => {
+        console.log("INGRESO A genericSave");
+        const { application, campos } = args.application;
+ 
+        try {
+            let columnas =  campos.map(field => {
+                return field.nombre
+            }).join();
+            let values = campos.map(field => {
+                return field.valor
+            }).join();
+            console.log(columnas);
+            console.log(values);
+            const result = await db.sequelize.query('CALL genericSave (:APPLICATION_ID, :COLUMNAS, :VALUES )',
+                { replacements: { APPLICATION_ID: application.id, COLUMNAS: columnas, VALUES: values } });
+
+            let newFields = result.map(field => {
+                return JSON.stringify(field);
+            });
+
+            const r =  {
+                //application,
+                campos : newFields
+            };
+            return [r];
+        } catch (error) {
+            throw error;
+        }
+    },
+
+    
+
+
 };
 
