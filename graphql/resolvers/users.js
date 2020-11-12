48b345307ba8834a8ba2b035810b0100fa178c29
andresverjan <andresverjan@hotmail.com>
2020-11-12 15:59:59 -0500

    Usuarios - Etiquetas - Idiomas

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,30 +1,46 @@
 const db = require("../../models");
 const helpers = require("../../helpers");
-const Application = db.application;
+const Users = db.users;
+const Idioma = db.idiomas;
+const Etiquetas = db.etiquetas;
 const Campo = db.campos;
 const Op = db.Sequelize.Op;
 
 module.exports = {
     login: async (args) => {
         console.log("INGRESO A getUserMenu");
-        const { username, password } = args.filter;
+        console.log(args);
+        const { username, password } = args;
         try {
-            const result = await db.sequelize.query('CALL login(:USERNAME, :PASSWORD)',
-                { replacements: { USERNAME: username, PASSWORD: password,  } });
 
-            let newFields = result.map(field => {
-                console.log(field);
-                return JSON.stringify(field);
-            });
-            const r = {
-                application,
-                campos: newFields
-            };
-            return [r];
+            const usr = await Users.findOne({ where: { USUARIO_LOGIN: username, USUARIO_PASSWORD: password } });
+            if (usr != null) {
+                const permisos = await db.sequelize.query('CALL login(:USERNAME, :USERPASS)',
+                    { replacements: { USERNAME: username, USERPASS: password, } });
+                let usrFinal = {
+                    ...usr.dataValues,
+                    PERMISOS : permisos
+                };
+                let idiomaId = usrFinal.IDIOMA_ID?usrFinal.IDIOMA_ID:1;
+                if (idiomaId != null ) {
+                    const idioma = await Idioma.findOne({ where: { ID:  idiomaId} });
+                    usrFinal = {
+                        ...usrFinal, 
+                        IDIOMA: idioma.dataValues
+                    };
+                    const etiquetas = await Etiquetas.findAll({ where: { IDIOMA_ID:  idiomaId} });
+                    usrFinal = {
+                        ...usrFinal, 
+                        ETIQUETAS: etiquetas
+                    };                    
+                }
+                console.log(usrFinal);
+                return usrFinal;
+            }
         } catch (error) {
             throw error;
         }
     },
 
-  
+
 };
