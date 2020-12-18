1a1987bf37cf9ae522bb041c535ea848dce4f77f
Luis Alvarez <lealvarezp@unac.edu.co>
2020-12-18 15:08:28 -0500

    correcciones de citas con fk

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
     acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
@@ -18,7 +18,7 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 console.log("****************************");
-console.log("Runing some Querys!....");
+console.log("Runing some Queries!....");
 const TIME_OUT = "SET GLOBAL connect_timeout=60000;";
 const INTERACTIVE_TIME_OUT = "SET GLOBAL interactive_timeout=28800;";
 const WAIT_TIME_OUT = "SET GLOBAL wait_timeout=28800;";
@@ -28,30 +28,33 @@ sequelize.query(TIME_OUT);
 sequelize.query(INTERACTIVE_TIME_OUT);
 sequelize.query(WAIT_TIME_OUT);
 
-sequelize.query(SHOW_VARIABLES).then(function(result){
+sequelize.query(SHOW_VARIABLES).then(function (result) {
   console.log(result);
 });
 console.log("DONE");
 console.log("****************************");
 
-db.tutorials    = require("../models/tutorial")(sequelize, Sequelize);
-db.comment      = require("../models/comment")(sequelize, Sequelize);
-db.etiquetas    = require("../models/etiquetas")(sequelize, Sequelize);
-db.users        = require("../models/user")(sequelize, Sequelize);
-db.idiomas      = require("../models/idioma")(sequelize, Sequelize);
-db.iconos       = require("../models/icono")(sequelize, Sequelize);
-db.tipocampos   = require("../models/tipocampos")(sequelize, Sequelize);
-db.mascara      = require("../models/mascara")(sequelize, Sequelize);
-db.application  = require("../models/applications")(sequelize, Sequelize);
-db.campos       = require("../models/applicationFieldsConfig")(sequelize, Sequelize);
-db.rol          = require("../models/rol")(sequelize, Sequelize);
-db.permiso      = require("../models/permiso")(sequelize, Sequelize);
-db.paciente     = require("../models/paciente")(sequelize, Sequelize);
-db.servicio     = require("../models/servicio")(sequelize, Sequelize);
-db.rolPermiso   = require("../models/rolpermiso")(sequelize, Sequelize);
-db.citas        = require("../models/citas")(sequelize, Sequelize);
-db.odontologos  = require("../models/odontologos")(sequelize, Sequelize);
-db.citaTrazabilidad = require("../models/citaTrazabilidad")(sequelize, Sequelize);
+db.tutorials = require("../models/tutorial")(sequelize, Sequelize);
+db.comment = require("../models/comment")(sequelize, Sequelize);
+db.etiquetas = require("../models/etiquetas")(sequelize, Sequelize);
+db.users = require("../models/user")(sequelize, Sequelize);
+db.idiomas = require("../models/idioma")(sequelize, Sequelize);
+db.iconos = require("../models/icono")(sequelize, Sequelize);
+db.tipocampos = require("../models/tipocampos")(sequelize, Sequelize);
+db.mascara = require("../models/mascara")(sequelize, Sequelize);
+db.application = require("../models/applications")(sequelize, Sequelize);
+db.campos = require("../models/applicationFieldsConfig")(sequelize, Sequelize);
+db.rol = require("../models/rol")(sequelize, Sequelize);
+db.permiso = require("../models/permiso")(sequelize, Sequelize);
+db.paciente = require("../models/paciente")(sequelize, Sequelize);
+db.servicio = require("../models/servicio")(sequelize, Sequelize);
+db.rolPermiso = require("../models/rolpermiso")(sequelize, Sequelize);
+db.citas = require("../models/citas")(sequelize, Sequelize);
+db.odontologos = require("../models/odontologos")(sequelize, Sequelize);
+db.citaTrazabilidad = require("../models/citaTrazabilidad")(
+  sequelize,
+  Sequelize
+);
 
 db.tutorials.hasMany(db.comment, { as: "comments" });
 db.comment.belongsTo(db.tutorials, {
@@ -59,12 +62,11 @@ db.comment.belongsTo(db.tutorials, {
   as: "tutorial",
 });
 
-db.application.hasMany(db.campos, { onDelete: 'CASCADE',
-                                    as: "fields" });
+db.application.hasMany(db.campos, { onDelete: "CASCADE", as: "fields" });
 
 db.campos.belongsTo(db.application, {
   foreignKey: "applicationId",
-  onDelete: 'CASCADE',
+  onDelete: "CASCADE",
   as: "application",
 });
 
@@ -84,36 +86,43 @@ db.rol.belongsToMany(db.permiso, {
   through: "rol_permiso",
   as: "permisos",
   foreignKey: "rol_id",
-  primaryKey: true
+  primaryKey: true,
 });
 
 db.permiso.belongsToMany(db.rol, {
   through: "rol_permiso",
   as: "roles",
   foreignKey: "permiso_id",
-  primaryKey: true
+  primaryKey: true,
 });
 
-db.odontologos.hasMany(db.citas, { onDelete: 'CASCADE',as: "citas" });
+db.odontologos.hasMany(db.citas, { onDelete: "CASCADE", as: "citas" });
 
 db.citas.belongsTo(db.odontologos, {
   foreignKey: "odontologoId",
-  onDelete: 'CASCADE',
+  onDelete: "CASCADE",
   as: "odontologos",
 });
 
 db.paciente.hasMany(db.citas, { as: "citas" });
 db.citas.belongsTo(db.paciente, {
   foreignKey: "pacienteId",
-  onDelete: 'CASCADE',
+  onDelete: "CASCADE",
   as: "paciente",
 });
 
 db.servicio.hasMany(db.citas, { as: "citas" });
 db.citas.belongsTo(db.servicio, {
   foreignKey: "servicioId",
-  onDelete: 'CASCADE',
+  onDelete: "CASCADE",
   as: "servicio",
+});
+
+db.users.hasMany(db.citas, { as: "citas" });
+db.citas.belongsTo(db.users, {
+  foreignKey: "usuarioId",
+  onUpdate: "CASCADE",
+  as: "usuarios",
 });
 
 db.status = require("../models/citaEstado")(sequelize, Sequelize);
