4a0e4a68c75a4a4c33dffff5c964c8c8b19c4778
Luis <lealvarezp@unac.edu.co>
2020-12-24 17:14:05 -0500

    fix id usuarios

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const Express = require("express");
 const ExpressGraphQL = require("express-graphql");
-const types = require('./graphql/schema/index')
-const genericResolver = require('./graphql/resolvers')
+const types = require("./graphql/schema/index");
+const genericResolver = require("./graphql/resolvers");
 var cors = require("cors");
 
-const {buildSchema } = require("graphql");
+const { buildSchema } = require("graphql");
 const schema = buildSchema(types);
 var app = Express();
-app.use('*', cors());
-app.use("/api", ExpressGraphQL({
-    schema:    schema,
+app.use("*", cors());
+app.use(
+  "/api",
+  ExpressGraphQL({
+    schema: schema,
     rootValue: genericResolver,
-    graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
 const db = require("./models");
 db.sequelize.sync({ force: false }).then(() => {
-    console.log("Drop and re-sync db.");
-  });
-const PORT = process.env.PORT || 3000
+  console.log("Drop and re-sync db.");
+});
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+  console.log(`Our app is running on port ${PORT}`);
 });
