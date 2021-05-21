e7c01aa63b77fd97ed1d05546aa9d92a9a3336b9
Luis Alvarez <lealvarezp@unac.edu.co>
2021-05-21 03:50:38 -0500

    completando crud

--- graphql/resolvers/egresos.js
+++ graphql/resolvers/egresos.js
@@ -14,4 +14,49 @@ module.exports = {
       throw error;
     }
   },
+  createEgresos: async (args) => {
+    try {
+      await Egresos.create(args.egreso).then((data) => {
+        console.log(data);
+      });
+      return `Egreso con T17Nivel1 = ${args.egreso.T17Nivel1} --> CREADO`;
+    } catch (error) {
+      throw error;
+    }
+  },
+  updateEgresos: async (args) => {
+    try {
+      await Egresos.update(args.egreso, {
+        where: { T17Nivel1: args.egreso.T17Nivel1 },
+      }).then((data) => {
+        console.log(data);
+      });
+      return `Egreso con T17Nivel1 = ${args.egreso.T17Nivel1} --> ACTUALIZADO`;
+    } catch (error) {
+      throw error;
+    }
+  },
+  deleteEgresos: async (args) => {
+    const { T17Nivel1 } = args.egreso;
+    try {
+      Egresos.destroy({
+        where: { T17Nivel1 },
+      })
+        .then((resCode) => {
+          if (resCode == 1) {
+            console.log('Egreso was deleted successfully!');
+          } else {
+            console.log(
+              `Cannot delete Egreso with T17Nivel1=${T17Nivel1}. Maybe Egreso was not found!`,
+            );
+          }
+        })
+        .catch((err) => {
+          console.log('Could not delete Egreso with T17Nivel1=' + T17Nivel1);
+        });
+      return 'Egreso fue borrado';
+    } catch (error) {
+      throw error;
+    }
+  },
 };
