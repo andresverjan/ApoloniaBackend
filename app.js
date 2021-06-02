6042c53ff0b168848d4d283a70991b6ae9b18146
Luis Alvarez <lealvarezp@unac.edu.co>
2021-06-02 05:54:02 -0500

    formato

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const Express = require("express");
-const ExpressGraphQL = require("express-graphql");
-const types = require("./graphql/schema/index");
-const genericResolver = require("./graphql/resolvers");
-var cors = require("cors");
+const Express = require('express');
+const ExpressGraphQL = require('express-graphql');
+const types = require('./graphql/schema/index');
+const genericResolver = require('./graphql/resolvers');
+var cors = require('cors');
 
-const { buildSchema } = require("graphql");
+const { buildSchema } = require('graphql');
 const schema = buildSchema(types);
 var app = Express();
-app.use("*", cors());
+app.use('*', cors());
 app.use(
-  "/api",
+  '/api',
   ExpressGraphQL({
     schema: schema,
     rootValue: genericResolver,
     graphiql: true,
-  })
+  }),
 );
 
-const db = require("./models");
+const db = require('./models');
 db.sequelize.sync({ force: false }).then(() => {
-  console.log("Drop and re-sync db.");
+  console.log('Drop and re-sync db.');
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
