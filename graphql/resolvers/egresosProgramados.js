72ae57754774f3a756f9072dcc51c2735ffe0c2b
Luis <lealvarezp@unac.edu.co>
2021-10-11 01:19:38 -0500

    ajustes egresos

--- graphql/resolvers/egresosProgramados.js
+++ graphql/resolvers/egresosProgramados.js
@@ -1,10 +1,51 @@
+const { Op } = require('sequelize');
+const moment = require('moment');
 const db = require('../../models');
+const helpers = require('../../helpers');
 const EgresosProgramados = db.egresosProgramados;
 
 module.exports = {
   egresosProgramados: async (args) => {
+    let where = {};
+    if (args.filter) {
+      where = helpers.getFilterFromObject(args.filter);
+      if (
+        args.filter.T17FechaFin &&
+        args.filter.T17FechaFin != '' &&
+        args.filter.T17FechaIni &&
+        args.filter.T17FechaIni != ''
+      ) {
+        where.where.push({
+          ['T17Fecha']: {
+            [Op.between]: [
+              moment(args.filter.T17FechaIni).toDate(),
+              moment(args.filter.T17FechaFin).toDate(),
+            ],
+          },
+        });
+      }
+      where.where = where.where.filter(
+        (e) =>
+          !(
+            e.hasOwnProperty('T17FechaIni') === true ||
+            e.hasOwnProperty('T17FechaFin') === true
+          ),
+      );
+    }
+
+    let egresos = await EgresosProgramados.findAll(where);
+    const totalRegistros = egresos.length;
+
+    if (args.pagination) {
+      const { limite: limit, pagina } = args.pagination;
+      const offset = limit * pagina;
+      where = { ...where, limit, offset };
+    }
     try {
-      return await EgresosProgramados.findAll();
+      egresosProgramados = await EgresosProgramados.findAll(
+        where,
+      );
+      return { totalRegistros, egresosProgramados };
     } catch (error) {
       throw error;
     }
@@ -17,6 +58,51 @@ module.exports = {
         console.log(data);
       });
       return `Egreso Programado con T17Factura = ${args.egresoProgramado.T17Factura} --> CREADO`;
+    } catch (error) {
+      throw error;
+    }
+  },
+  updateEgresosProgramados: async (args) => {
+    try {
+      await EgresosProgramados.update(
+        args.egresoProgramado,
+        {
+          where: {
+            T17Factura: args.egresoProgramado.T17Factura,
+          },
+        },
+      ).then((data) => {
+        console.log(data);
+      });
+      return `Egreso Programado con T17Factura = ${args.egresoProgramado.T17Factura} --> ACTUALIZADO`;
+    } catch (error) {
+      throw error;
+    }
+  },
+  deleteEgresosProgramados: async (args) => {
+    const { T17Factura } = args.egresoProgramado;
+    try {
+      EgresosProgramados.destroy({
+        where: { T17Factura },
+      })
+        .then((resCode) => {
+          if (resCode == 1) {
+            console.log(
+              'Egreso Programado was deleted successfully!',
+            );
+          } else {
+            console.log(
+              `Cannot delete Egreso Programado with T17Factura=${T17Factura}. Maybe Egreso Programado was not found!`,
+            );
+          }
+        })
+        .catch((err) => {
+          console.log(
+            'Could not delete Egreso Programado with T17Factura=' +
+              T17Factura,
+          );
+        });
+      return 'Egreso Programado fue borrado';
     } catch (error) {
       throw error;
     }
