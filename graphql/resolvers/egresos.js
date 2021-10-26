992f1587e065b941d20c414335f5603fe04f42e4
Luis <lealvarezp@unac.edu.co>
2021-10-25 22:32:37 -0500

    listar egresos pagado

--- graphql/resolvers/egresos.js
+++ graphql/resolvers/egresos.js
@@ -1,10 +1,47 @@
 const db = require('../../models');
+const helpers = require('../../helpers');
+const moment = require('moment');
 const Egresos = db.egresos;
 
 module.exports = {
   egresos: async (args) => {
+    let where = {};
+    if (args.filter) {
+      where = helpers.getFilterFromObject(args.filter);
+      if (
+        args.filter.T17FechaFin &&
+        args.filter.T17FechaFin != '' &&
+        args.filter.T17FechaIni &&
+        args.filter.T17FechaIni != ''
+      ) {
+        where.where.push({
+          ['T17Fecha']: {
+            [Op.between]: [
+              moment(args.filter.T17FechaIni).toDate(),
+              moment(args.filter.T17FechaFin).toDate(),
+            ],
+          },
+        });
+      }
+      where.where = where.where.filter(
+        (e) =>
+          !(
+            e.hasOwnProperty('T17FechaIni') === true ||
+            e.hasOwnProperty('T17FechaFin') === true
+          ),
+      );
+    }
+
+    let egresos = await Egresos.findAll(where);
+    const totalRegistros = egresos.length;
+    if (args.pagination) {
+      const { limite: limit, pagina } = args.pagination;
+      const offset = limit * (pagina - 1);
+      where = { ...where, limit, offset };
+    }
     try {
-      return await Egresos.findAll();
+      const egresos = await Egresos.findAll(where);
+      return { totalRegistros, egresos };
     } catch (error) {
       throw error;
     }
