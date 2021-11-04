5ef52f62797659a7e45c9ba9a0bee9205722efd6
Harold Andres Verjan Diaz <andresverjan@hotmail.com>
2021-11-04 14:10:20 -0500

    MEJORAS SERVICIO getHappyBirthdayList ORDER Y DAY

--- graphql/resolvers/dashboard.js
+++ graphql/resolvers/dashboard.js
@@ -16,10 +16,20 @@ module.exports = {
   },
   getHappyBirthdayList: async (args) => {
     let startOfMonth = moment().startOf('month').format('MM');
+    let startOfDay = moment().startOf('day').format('DD');
     try {
       let objectFilter = {};
-      objectFilter.where= [];
-      objectFilter.where= db.sequelize.where (db.sequelize.fn('MONTH', db.sequelize.col('FechaNacimiento')) , startOfMonth);
+      objectFilter.where = [];
+      objectFilter.where =
+      {
+        [Op.and]: [
+          db.sequelize.where(db.sequelize.fn('MONTH', db.sequelize.col('FechaNacimiento')), startOfMonth),
+          db.sequelize.where(db.sequelize.fn('DAY', db.sequelize.col('FechaNacimiento')), { [Op.gte]: startOfDay})
+        ]
+      }
+      objectFilter.order = [
+        [db.sequelize.fn('DAY', db.sequelize.col('FechaNacimiento')), 'ASC'],
+      ];
       return list = await Pacientes.findAll(objectFilter);
     } catch (error) {
       throw error;
