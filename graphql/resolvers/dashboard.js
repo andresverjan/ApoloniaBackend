fe5f371f37f1a36c9a36b30b53ff62c182c52051
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-11-18 12:29:15 -0500

    numero de citas atendidas en un mes

--- graphql/resolvers/dashboard.js
+++ graphql/resolvers/dashboard.js
@@ -2,22 +2,11 @@ const db = require("../../models");
 const Pacientes = db.paciente;
 const moment = require('moment');
 const { Op } = require('sequelize');
+const paciente = require("../../models/paciente");
+const Citas = db.citas;
+const Status = db.status;
 
 module.exports = {
-    getPacienteSexo: async () => {
-        try {
-            return await Pacientes.findAll({
-                attributes: ['Sexo', [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'consult']],        
-                group: ['paciente.Sexo'],
-            }).then((data) => {
-                return data.map(pacientes => {
-                    return pacientes.dataValues;
-                });
-            })
-        } catch (error) {
-            throw error;
-        }
-    },
   getNumPacientes: async (args) => {
     console.log("argumentos ");
     console.log(args);
@@ -30,21 +19,40 @@ module.exports = {
   },
   getHappyBirthdayList: async (args) => {
     let startOfMonth = moment().startOf('month').format('MM');
-    let startOfDay = moment().startOf('day').format('DD');
     try {
       let objectFilter = {};
       objectFilter.where = [];
-      objectFilter.where =
-      {
-        [Op.and]: [
-          db.sequelize.where(db.sequelize.fn('MONTH', db.sequelize.col('FechaNacimiento')), startOfMonth),
-          db.sequelize.where(db.sequelize.fn('DAY', db.sequelize.col('FechaNacimiento')), { [Op.gte]: startOfDay})
-        ]
-      }
-      objectFilter.order = [
-        [db.sequelize.fn('DAY', db.sequelize.col('FechaNacimiento')), 'ASC'],
-      ];
+      objectFilter.where = db.sequelize.where(db.sequelize.fn('MONTH', db.sequelize.col('FechaNacimiento')), startOfMonth);
       return list = await Pacientes.findAll(objectFilter);
+    } catch (error) {
+      throw error;
+    }
+  },
+  getPacienteSexo: async () => {
+    try {
+      return await Pacientes.findAll({
+        attributes: ['Sexo', [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'consult']],
+        group: ['paciente.Sexo'],
+      }).then((data) => {
+        return data.map(pacientes => {
+          return pacientes.dataValues;
+        });
+      })
+    } catch (error) {
+      throw error;
+    }
+  },
+  getCitasAtendidas: async () => {
+    let citasMonth = moment().startOf('month').format('MM');
+    try {
+      return citaAte = await Citas.findAll({
+        where:{status:'2'},
+        attributes: ['status','start', [db.sequelize.fn('MONTH', db.sequelize.col('start')),citasMonth]],
+      }).then((data) => {
+        return data.map(result => {
+          return result.dataValues;
+        });
+      });
     } catch (error) {
       throw error;
     }
