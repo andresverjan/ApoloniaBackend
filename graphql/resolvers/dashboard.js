000dd9946f129b95091a8fe500635e003052cab0
HAROLD VERJAN <andresverjandiaz@gmail.com>
2021-11-18 15:10:33 -0500

    Mejoras Citas Atentidas por año.

--- graphql/resolvers/dashboard.js
+++ graphql/resolvers/dashboard.js
@@ -28,6 +28,7 @@ module.exports = {
       throw error;
     }
   },
+
   getPacienteSexo: async () => {
     try {
       return await Pacientes.findAll({
@@ -42,38 +43,20 @@ module.exports = {
       throw error;
     }
   },
-  getCitasAtendidas: async () => {
-    let citasMonth = moment().startOf('month').format('MM');
-    try {
-      return citaAte = await Citas.findAll({
-        where:{status:'2'},
-        attributes: ['status','start', [db.sequelize.fn('MONTH', db.sequelize.col('start')),citasMonth]],
-      }).then((data) => {
-        return data.map(result => {
-          return result.dataValues;
-        });
-      });
-    } catch (error) {
-      throw error;
-    }
-  },
 
-
-  /*
-   * Permite obtener la cantidad de citas que se atendieron en el mes actual.
-  */
-  getNumCitasAtendidasByMonth: async () => {
-    let citasMonth = moment().startOf('month').format('MM');
-    console.log(citasMonth);
+  getNumCitasAtendidasToday: async () => {
+    let time = moment().startOf('day').format('dd');
     try {
       let objectFilter = {};
       objectFilter.where = [];
       objectFilter.where = {
-        'status' : '2',
-        [Op.and]: db.sequelize.where(db.sequelize.fn('MONTH', db.sequelize.col('start')), citasMonth)
+        'status': '2',
+        [Op.and]: db.sequelize.where(db.sequelize.fn('DAY', db.sequelize.col('start')), time)
       };
-      return await Citas.findOne({
-        where:  objectFilter.where,
+
+
+      return citaAte = await Citas.findAll({
+        where: objectFilter.where,
         attributes: [[db.sequelize.fn('COUNT', db.sequelize.col('*')), 'count']],
       }).then((data) => {
         let response = {
@@ -87,30 +70,58 @@ module.exports = {
   },
 
 
-    /*
-    * Permite obtener la cantidad de citas que se atendieron cada mes en el año actual
-    */
-    getNumCitasAtendidasbyMonthThisYear: async () => {
-      let year = moment().startOf('year').format('YYYY');
-      try {
-        let objectFilter = {};
-        objectFilter.where = [];
-        objectFilter.where = {
-          'status' : '2'
+  /*
+   * Permite obtener la cantidad de citas que se atendieron en el mes actual.
+  */
+  getNumCitasAtendidasThisMonth: async () => {
+    let citasMonth = moment().startOf('month').format('MM');
+    console.log(citasMonth);
+    try {
+      let objectFilter = {};
+      objectFilter.where = [];
+      objectFilter.where = {
+        'status': '2',
+        [Op.and]: db.sequelize.where(db.sequelize.fn('MONTH', db.sequelize.col('start')), citasMonth)
+      };
+      return await Citas.findOne({
+        where: objectFilter.where,
+        attributes: [[db.sequelize.fn('COUNT', db.sequelize.col('*')), 'count']],
+      }).then((data) => {
+        let response = {
+          ...data.dataValues
         };
-        return await Citas.findAll({
-          where:  objectFilter.where,
-          attributes: [[db.sequelize.fn('MONTH', db.sequelize.col('start')), 'MONTH'],[db.sequelize.fn('COUNT', db.sequelize.col('*')), 'count']],
-          group:  ['MONTH']
-        }).then((data) => {
-          let response = {
-            ...data.dataValues
-          };
-          return response;
+        return response;
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
+
+
+  /*
+  * Permite obtener la cantidad de citas que se atendieron cada mes en el año actual
+  */
+  getNumCitasAtendidasbyMonthThisYear: async () => {
+    let year = moment().startOf('year').format('YYYY');
+    try {
+      let objectFilter = {};
+      objectFilter.where = [];
+      objectFilter.where = {
+        'status': '2',
+        [Op.and]: db.sequelize.where(db.sequelize.fn('YEAR', db.sequelize.col('start')), year)
+      };
+      return await Citas.findAll({
+        where: objectFilter.where,
+        attributes: [[db.sequelize.fn('MONTH', db.sequelize.col('start')), 'MONTH'], [db.sequelize.fn('COUNT', db.sequelize.col('*')), 'count']],
+        group: ['MONTH']
+      }).then((data) => {
+        return data.map(obj => {
+          return obj.dataValues;
         });
-      } catch (error) {
-        throw error;
-      }
-    },
+      });
+    } catch (error) {
+      throw error;
+    }
+  },
 
 };
