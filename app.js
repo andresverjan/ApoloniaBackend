fcabe6e8ebf555860b4bba2a2f11045bebd20aa9
vega178 <estebanvegapatio@gmail.com>
2022-03-16 11:31:25 -0500

    Request husbpot get deals

--- app.js
+++ app.js
@@ -1,72 +1,200 @@
-const Express = require('express');
-const ExpressGraphQL = require('express-graphql');
-const compression = require('compression');
-const types = require('./graphql/schema/index');
-const genericResolver = require('./graphql/resolvers');
-var cors = require('cors');
-const { buildSchema } = require('graphql');
-const schema = buildSchema(types);
+require('dotenv').config();
+const express = require('express');
+const request = require('request-promise-native');
+const NodeCache = require('node-cache');
+const session = require('express-session');
+const opn = require('open');
+const app = express();
 
-const open = require('open');
-const hubspot = require('@hubspot/api-client')
+const PORT = 3000;
 
-const developerApiKey = '9d2d376d-abb2-472f-9271-9d65fa9eeec1';
-const hubspotClient = new hubspot.Client({ apiKey: developerApiKey })
-const clientId = '9e946e8e-2c1e-4a13-bbc2-86016f8058a1'
-const clientSecret= '71f11326-41fa-4305-a726-89cc9a07f519'
-const redirectUri = 'http://localhost:3000/oauth-callback'
+const refreshTokenStore = {};
+const accessTokenCache = new NodeCache({ deleteOnExpire: true });
 
-const scope = "oauth crm.objects.contacts.read crm.schemas.contacts.read crm.objects.deals.read crm.schemas.deals.read";
-
-const uri = hubspotClient.oauth.getAuthorizationUrl(clientId, redirectUri, scope)
-open(uri);
-console.log("Url hubspot " +  uri);
-
-var app = Express();
-app.use('*', cors());
-app.use(compression());
-
-app.get('/oauth-callback' ,  ( { query: {code} }, res ) =>{
-  console.log("ESTA EN EL CALLBACKKK");
-  console.log(code);
-  return hubspotClient.oauth.tokensApi.createToken(
-    'authorization_code',
-    code, 
-    redirectUri,
-    clientId,
-    clientSecret,
-).then(res => {
-  console.log("response from Create token");
-  console.log(res);
-  call();
-})
-
-} );
-
-async function  call (){
-  const allContacts = await hubspotClient.crm.deals.getAll()
-  console.log("VAMOS A OBTENER TODOS LOS CONTACTOS");
-  console.log(allContacts);
+if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
+    throw new Error('Missing CLIENT_ID or CLIENT_SECRET environment variable.')
 }
 
-//Commented for POC_API.
-/*app.use(
-  '/api',
-  ExpressGraphQL({
-    schema: schema,
-    rootValue: genericResolver,
-    graphiql: true,
-  }),
-);*/
+//===========================================================================//
+//  HUBSPOT APP CONFIGURATION
+//
+//  All the following values must match configuration settings in your app.
+//  They will be used to build the OAuth URL, which users visit to begin
+//  installing. If they don't match your app's configuration, users will
+//  see an error page.
 
-//Commented for POC_API.
-/*const db = require('./models');
-db.sequelize.sync({ force: false }).then(() => {
-  console.log('Drop and re-sync db.');
-});*/
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Our app is running on port ${PORT}`);
+// Replace the following with the values from your app auth config, 
+// or set them as environment variables before running.
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+// Scopes for this app will default to `crm.objects.contacts.read`
+// To request others, set the SCOPE environment variable instead
+let SCOPES = ['crm.objects.contacts.read'];
+if (process.env.SCOPE) {
+    SCOPES = (process.env.SCOPE.split(/ |, ?|%20/)).join(' ');
+}
+
+// On successful install, users will be redirected to /oauth-callback
+const REDIRECT_URI = `http://localhost:${PORT}/oauth-callback`;
+
+//===========================================================================//
+
+// Use a session to keep track of client ID
+app.use(session({
+  secret: Math.random().toString(36).substring(2),
+  resave: false,
+  saveUninitialized: true
+}));
+ 
+//================================//
+//   Running the OAuth 2.0 Flow   //
+//================================//
+
+// Step 1
+// Build the authorization URL to redirect a user
+// to when they choose to install the app
+const authUrl =
+  'https://app.hubspot.com/oauth/authorize' +
+  `?client_id=${encodeURIComponent(CLIENT_ID)}` + // app's client ID
+  `&scope=${encodeURIComponent(SCOPES)}` + // scopes being requested by the app
+  `&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`; // where to send the user after the consent page
+
+// Redirect the user from the installation page to
+// the authorization URL
+app.get('/install', (req, res) => {
+  console.log('');
+  console.log('=== Initiating OAuth 2.0 flow with HubSpot ===');
+  console.log('');
+  console.log("===> Step 1: Redirecting user to your app's OAuth URL");
+  res.redirect(authUrl);
+  console.log('===> Step 2: User is being prompted for consent by HubSpot');
 });
 
+// Step 2
+// The user is prompted to give the app access to the requested
+// resources. This is all done by HubSpot, so no work is necessary
+// on the app's end
 
+// Step 3
+// Receive the authorization code from the OAuth 2.0 Server,
+// and process it based on the query parameters that are passed
+app.get('/oauth-callback', async (req, res) => {
+  console.log('===> Step 3: Handling the request sent by the server');
+
+  // Received a user authorization code, so now combine that with the other
+  // required values and exchange both for an access token and a refresh token
+  if (req.query.code) {
+    console.log('       > Received an authorization token');
+
+    const authCodeProof = {
+      grant_type: 'authorization_code',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
+      code: req.query.code
+    };
+
+    // Step 4
+    // Exchange the authorization code for an access token and refresh token
+    console.log('===> Step 4: Exchanging authorization code for an access token and refresh token');
+    const token = await exchangeForTokens(req.sessionID, authCodeProof);
+    if (token.message) {
+      return res.redirect(`/error?msg=${token.message}`);
+    }
+
+    // Once the tokens have been retrieved, use them to make a query
+    // to the HubSpot API
+    res.redirect(`/`);
+  }
+});
+
+//==========================================//
+//   Exchanging Proof for an Access Token   //
+//==========================================//
+
+const exchangeForTokens = async (userId, exchangeProof) => {
+  try {
+    const responseBody = await request.post('https://api.hubapi.com/oauth/v1/token', {
+      form: exchangeProof
+    });
+    // Usually, this token data should be persisted in a database and associated with
+    // a user identity.
+    const tokens = JSON.parse(responseBody);
+    refreshTokenStore[userId] = tokens.refresh_token;
+    accessTokenCache.set(userId, tokens.access_token, Math.round(tokens.expires_in * 0.75));
+
+    console.log('       > Received an access token and refresh token');
+    return tokens.access_token;
+  } catch (e) {
+    console.error(`       > Error exchanging ${exchangeProof.grant_type} for access token`);
+    return JSON.parse(e.response.body);
+  }
+};
+
+const refreshAccessToken = async (userId) => {
+  const refreshTokenProof = {
+    grant_type: 'refresh_token',
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    redirect_uri: REDIRECT_URI,
+    refresh_token: refreshTokenStore[userId]
+  };
+  return await exchangeForTokens(userId, refreshTokenProof);
+};
+
+const getAccessToken = async (userId) => {
+  // If the access token has expired, retrieve
+  // a new one using the refresh token
+  if (!accessTokenCache.get(userId)) {
+    console.log('Refreshing expired access token');
+    await refreshAccessToken(userId);
+  }
+  return accessTokenCache.get(userId);
+};
+
+const isAuthorized = (userId) => {
+  return refreshTokenStore[userId] ? true : false;
+};
+
+const getProyectDeals = async(accessToken) => {
+  const headers = {
+    Authorization: `Bearer ${accessToken}`,
+    'Content-Type': 'application/json'
+  };
+  try {
+    const dealResponse = await request.get('https://api.hubapi.com/deals/v1/deal/paged?includeAssociations=true&properties=num_associated_contacts&properties=dealname&properties=amount', {
+      headers: headers
+    })
+    return JSON.parse(dealResponse).deals; 
+  } catch (error) {
+    console.error('The user has not deals');
+    return JSON.parse(error.response.body);
+  }
+};
+
+app.get('/', async (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h2>HubSpot OAuth 2.0 Quickstart App</h2>`);
+  if (isAuthorized(req.sessionID)) {
+    const accessToken = await getAccessToken(req.sessionID);
+    const deals = await getProyectDeals(accessToken); 
+    deals.forEach(deal => {
+      res.write(`<h4>Deal Name : ${deal.properties.dealname.value}</h4>`);
+      res.write(`<h4>Deal amount: : ${deal.properties.amount.value}</h4>`);
+      res.write(`<h4>Num Associated Contact : ${deal.properties.num_associated_contacts.value}</h4>`);
+    });
+  } else {
+    res.write(`<a href="/install"><h3>Install the app</h3></a>`);
+  }
+  res.end();
+});
+
+app.get('/error', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.write(`<h4>Error: ${req.query.msg}</h4>`);
+  res.end();
+});
+
+app.listen(PORT, () => console.log(`=== Starting your app on http://localhost:${PORT} ===`));
+opn(`http://localhost:${PORT}`);
