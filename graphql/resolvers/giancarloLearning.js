69057eab86ff9e46a10e3afa68c358046acb0722
GiancarloCabrera <giancarlo.cabrera@unac.edu.co>
2022-03-16 11:44:01 -0500

    Implementación mutations

--- graphql/resolvers/giancarloLearning.js
+++ graphql/resolvers/giancarloLearning.js
@@ -10,4 +10,51 @@ module.exports = {
             throw error;
         }
     },
+    createGiancarloLearning: async (args) => {
+        try {
+            return await Gl.create(args.tablaObject).then((data) =>{
+                console.log(data);
+                return data;
+            })
+        } catch (error) {
+            throw error;
+        }
+    },
+    deleteGiancarloLearning: async (args) => {
+        console.log("INGRESO A DELETE GIANCARLO LEARNING");
+        const { id } = args.tablaObject;
+        try {
+            Gl.destroy({
+                where: {id: id},
+            })
+                .then((num) =>
+                {
+                    if(num == 1){
+                        console.log("giancarloLearning was deleted successfully");
+                    }else{
+                        console.log(
+                            `Cannot delete Etiquetas with id=${id}. Maybe Tutorial was not found!`
+                        );
+                    }
+                })
+                .catch((err) => {
+                    console.log("Could not delete Tutorial with id=" + id);
+                });
+                return (list = await Gl.findAll());
+        } catch (error) {
+            throw error;
+        }
+    },
+    updateGiancarloLearning: async (args) => {
+        console.log("INGRESO giancarloLearningUpdate");
+        try {
+            return await Gl.update(args.tablaObject, {
+                where: {id: args.tablaObject.id},
+            }).then((data) => {
+                console.log(data);
+            })
+        } catch (error) {
+            throw error;
+        }
+    }
 };
